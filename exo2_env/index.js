5c1c1246ada6ca0d20aaa59baa39c41544406e17
Samuel <samuel.louismarie@gmail.com>
2022-04-28 17:06:04 +0200

    final commit

--- exo2_env/index.js
+++ exo2_env/index.js
@@ -1,3 +1,4 @@
-require("dotenv").config();
-
-console.log(process.env.UNEVARIABLE);
+require('dotenv').config();
+if (process.env.MAVARIABLE) {
+  console.log(process.env.MAVARIABLE);
+} else console.log('MAVARIABLE is not defined');
